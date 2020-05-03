8d4a5c574a2a56804fb979ee7a5e158c8ce9380f
rajan savaliya <rajanpatel99094@gmail.com>
2020-05-03 23:26:06 +0530

    q

--- pages/post.js
+++ pages/post.js
@@ -10,7 +10,7 @@ const Post = ({ router }) => (
       qqqqqqqqqqqqqqqqqqqqqqq
       qqqqqqqqqqqqqqqqqqqqqqqq
       qqqqqqqqqqqqqqqqqqqqqqqq
-      qqqqqqqqqqqqqqq
+      qqqqqqqqqqqqqqqq
     </p>
   </Layout>
 );
